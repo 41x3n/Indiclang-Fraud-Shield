7b8383c3af0e1a94b209e375979099f736118a8c
Anindya Chowdhury <anindyac@Anindyas-MacBook-Pro.local>
2025-05-14 02:40:21 +0530

    feat: Gitlab CI

--- apps/functions/hello-1/index.ts
+++ apps/functions/hello-1/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import { HttpFunction } from '@google-cloud/functions-framework';
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Function 11 (Express)!');
+    res.send('Hello from Function 1 (Express)!');
 });
 
-export default app;
+// Cloud Function handler for Google Cloud Functions Gen2
+export const defaultHandler: HttpFunction = (req, res) => {
+    return app(req, res);
+};
